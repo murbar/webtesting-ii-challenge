86270918087193a1b053e4daaee323c1032411a3
Joel Bartlett <joel@murbar.com>
2019-04-09 16:48:22 -0400

    Add useScoreboard

---
+++ src/useScoreboard.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const useScoreboard = () => {
+  const [data, setData] = useState({
+    strikes: 0,
+    balls: 0,
+    hits: 0
+  });
+
+  const recordBall = () => {
+    setData(prevData => {
+      if (prevData.balls === 3) return { ...prevData, balls: 0 };
+
+      return { ...prevData, ball: prevData.ball + 1 };
+    });
+  };
+
+  const recordStrike = () => {
+    setData(prevData => {
+      if (prevData.strikes === 2) return { ...prevData, strikes: 0 };
+
+      return { ...prevData, strikes: prevData.strikes + 1 };
+    });
+  };
+
+  const recordFoul = () => {
+    setData(prevData => {
+      if (prevData.strikes < 2) return { ...prevData, strikes: prevData.strikes + 1 };
+
+      return { ...prevData };
+    });
+  };
+
+  const recordHit = () => {
+    setData(prevData => {
+      return { ...prevData, hits: prevData.hits + 1 };
+    });
+  };
+
+  return { data, recordStrike, recordBall, recordFoul, recordHit };
+};
+
+export default useScoreboard;
